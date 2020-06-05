3448b997d23c812f6998b32f36472e295de1a33c
Jeff Adams <jeff.c.adams@gmail.com>
2020-06-05 16:26:14 -0400

    Add rgbmatrix library

--- timer.py
+++ timer.py
@@ -2,7 +2,7 @@ import time
 
 class Timer:
     def __init__(self):
-        self._start_time = time.perf_counter()
+        self._start_time = time.perf_counter() - 60.0
         
     def reset(self):
         self._start_time = time.perf_counter()
